f15d8ccde8dd069972c414faa4c682b3ea12a89d
angelo <angelodelperez@gmail.com>
2023-09-24 09:50:49 -0500

    responsive arreglado

--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 import searchIcon from "../assets/searchIcon.svg";
+import { useState } from "react";
 function Buscador() {
+  const [buscar, setBuscar] = useState("");
+  const almacenarBusqueda = (e) => {
+    setBuscar(e.target.value);
+  };
   return (
-    <div className="flex items-center bg-blue-200 gap-2 rounded-full">
+    <form
+      action=""
+      className="flex items-center bg-blue-200 rounded-full w-3/4 justify-around"
+    >
       <img src={searchIcon} className="w-11 h-4/5" />
-      <form action="">
-        <input type="text" name="Buscador"  className="inline-block h-10 bg-inherit border-x-2 border-cyan-50"/>
-        <button type="submit" className="mx-3">Buscar</button>
-      </form>
-    </div>
+      <input
+        type="text"
+        name="Buscador"
+        onChange={almacenarBusqueda}
+        className="inline-block h-10 bg-inherit border-x-2 border-cyan-50 w-3/4"
+      />
+      <button type="submit" className="mx-3">
+        Buscar
+      </button>
+    </form>
   );
 }
 
